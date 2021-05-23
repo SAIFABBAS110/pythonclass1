ec4af2678cab55531689a332f6856d2a8673b8bb
SAIFABBAS110 <84672702+SAIFABBAS110@users.noreply.github.com>
2021-05-24 00:08:13 +0530

    Add files via upload

---
+++ pythonclass1.py
@@ -0,0 +1,48 @@
+
+
+'''TYPE CONVERSION
+int
+float
+str
+bool'''
+
+
+
+a=100
+print(a, type(a))
+b= float(a)
+print(b)
+print(type(b))
+c=str(a)
+print(c)
+print(type(c))
+
+a=100.01
+print(a)
+print(type(a))
+b=str(a)
+print(b)
+print(type(b))
+c= int(a)
+print(c)
+print(type(c))
+
+a= 'abbas'
+print(a)
+print(type(a))
+b= bool(a)
+print(b)
+print(type(b))
+
+a=True
+print(a)
+print(type(a))
+b= str(a)
+print(b)
+print(type(b))
+c= int(a)
+print(c)
+print(type(c))
+d= float(a)
+print(d)
+print(type(d))
